880821f3b747735a73c941f32632f2ff76f2dcb4
Yuliia Trachova <YUTR@simcorp.com>
2023-02-22 15:41:31 +0100

    removed unnecessary comments and moved enum type into another file

---
+++ ConsoleAppHomeTask/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppHomeTask
+{
+    enum TransactionType { Buy, Sell }
+    class Class1
+    {
+    }
+}
